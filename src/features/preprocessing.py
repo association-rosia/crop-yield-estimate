19210df54200d3d5d0df2596f6c1dbae2da0cf9a
Louis REBERGA <louis.reberga@gmail.com>
2023-10-09 22:47:27 +0200

    init proprecessing transformers classes

---
+++ src/features/preprocessing.py
@@ -0,0 +1,45 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class ColumnListSplitter(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return
+
+    def transform(self, X, y=None):
+        return
+
+
+class CyclicalTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return
+
+    def transform(self, X, y=None):
+        return
+
+
+class OneHotEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return
+
+    def transform(self, X, y=None):
+        return
+
+
+class MissingValuesFiller(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return
+
+    def transform(self, X, y=None):
+        return
