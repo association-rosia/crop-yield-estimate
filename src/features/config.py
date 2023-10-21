bca675fe8f8be7b3b29f1f6f651a580a49090077
Louis REBERGA <louis.reberga@gmail.com>
2023-10-21 15:21:33 +0200

    add sweep configs

--- src/features/config.py
+++ src/features/config.py
@@ -6,16 +6,14 @@ sys.path.append(os.curdir)
 
 from src.config import BaseConfig
 
+
 class CYEConfigPreProcessor(BaseConfig):
-    def __init__(
-        self,
-        delna_thr=50,
-        fill_mode='none',
-        normalisation=True,
-        *args: Any,
-        **kwargs: Any
-    ) -> None:
-        
+    def __init__(self,
+                 delna_thr=50,
+                 fill_mode='none',
+                 normalisation=True,
+                 *args: Any, **kwargs: Any) -> None:
+        super().__init__()
         self.delna_thr = delna_thr
         self.fill_mode = fill_mode
         self.normalisation = normalisation
