f58b434cd35678599217ae9f15d22e528c5c2207
Louis REBERGA <louis.reberga@gmail.com>
2023-11-05 19:43:11 +0100

    add wandb project

--- src/data/train_great_model.py
+++ src/data/train_great_model.py
@@ -8,6 +8,8 @@ from src.constants import get_constants
 
 cst = get_constants()
 
+os.environ['WANDB_PROJECT'] = 'crop-yield-forecasting'
+
 data_path = os.path.join(cst.path_interim_data, 'Train_GReaT.csv')
 data = pd.read_csv(data_path)
 
