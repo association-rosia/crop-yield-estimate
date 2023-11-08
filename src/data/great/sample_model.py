4227b85e2554e8a1374b38311ef49b7f4aab968c
Louis REBERGA <louis.reberga@gmail.com>
2023-11-08 20:35:28 +0100

    max_length=2000

--- src/data/great/sample_model.py
+++ src/data/great/sample_model.py
@@ -13,8 +13,8 @@ llm = 'DistilGPT2'
 load_path = os.path.join(cst.path_models, llm)
 great = GReaT.load_from_dir(load_path)
 
-n_samples = 2000
-samples = great.sample(n_samples=n_samples)
+n_samples = 1000
+samples = great.sample(n_samples=n_samples, max_length=2000)
 
 now = int(time.time())
 save_path = os.path.join(cst.path_interim_data, f'{now}-GReaTSamples{llm}-{n_samples}.csv')
