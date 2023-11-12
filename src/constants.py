a48b54d4101968f3743f5ab7c633186f1e47d9dd
Louis REBERGA <louis.reberga@gmail.com>
2023-11-12 19:07:26 +0100

    add one_hot_decoding

--- src/constants.py
+++ src/constants.py
@@ -15,8 +15,9 @@ class CYEConstants:
 
     def __init__(self) -> None:
         # Paths to data directories
-        self.path_raw_data = os.path.join('data', 'raw')
         self.path_interim_data = os.path.join('data', 'interim')
+        self.path_processed_data = os.path.join('data', 'processed')
+        self.path_raw_data = os.path.join('data', 'raw')
 
         self.file_data_train = os.path.join(self.path_raw_data, 'Train.csv')
         self.file_data_test = os.path.join(self.path_raw_data, 'Test.csv')
