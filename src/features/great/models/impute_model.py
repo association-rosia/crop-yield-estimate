cad5b0d97af91b981b35f6095520f7a2fce652de
Louis REBERGA <louis.reberga@gmail.com>
2023-11-28 21:26:18 +0100

    update cst for colab

--- src/features/great/models/impute_model.py
+++ src/features/great/models/impute_model.py
@@ -9,8 +9,8 @@ from src.features.great.models.great import GReaT
 
 cst = get_constants()
 
-cst.path_interim_data = 'drive/MyDrive/20 - ROSIA/10 - Projets/03 - Digital Green Crop Yield Estimate Challenge/data/'
-print(cst.path_interim_data)
+cst.path_interim_data = 'drive/MyDrive/20 - ROSIA/10 - Projets/03 - Digital Green Crop Yield Estimate Challenge/data'
+cst.path_models = 'drive/MyDrive/20 - ROSIA/10 - Projets/03 - Digital Green Crop Yield Estimate Challenge/models'
 
 path_model = os.path.join(cst.path_models, 'GPT2Impute')
 great = GReaT.load_from_dir(path_model)
@@ -20,12 +20,12 @@ file_data_test = os.path.join(cst.path_interim_data, 'TestToImpute.csv')
 target_column = 'Yield'
 
 X_train = pd.read_csv(file_data_train)
-X_train_imputed = great.impute(X_train, temperature=1, max_length=1024, max_retries=100, device='mps')
+X_train_imputed = great.impute(X_train, temperature=1, max_length=1024, max_retries=100)
 save_path = os.path.join(cst.path_interim_data, 'TrainImputed.csv')
 X_train_imputed.to_csv(save_path, index=False)
 
 X_test = pd.read_csv(file_data_test)
-X_test_imputed = great.impute(X_test, temperature=1, max_length=1024, max_retries=100, device='mps')
+X_test_imputed = great.impute(X_test, temperature=1, max_length=1024, max_retries=100)
 save_path = os.path.join(cst.path_interim_data, 'TestImputed.csv')
 X_test_imputed.to_csv(save_path, index=False)
 
