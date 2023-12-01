773730ab3dc0692ae138597894306bb63d149df9
Louis REBERGA <louis.reberga@gmail.com>
2023-12-01 21:35:11 +0100

    update delete_run.py

--- delete_run.py
+++ delete_run.py
@@ -1,8 +1,14 @@
 import wandb
 from tqdm import tqdm
 
-api = wandb.Api(timeout=600)
-runs = api.runs('association-rosia/crop-yield-estimate', per_page=1000)
+api = wandb.Api(timeout=60)
+runs = api.runs('association-rosia/crop-yield-estimate', per_page=500)
 
-for run in tqdm(runs):
-    run.delete()
+while runs.length > 0:
+    try:
+        for run in tqdm(runs):
+            run.delete()
+    except Exception as e:
+        print(e)
+        api = wandb.Api(timeout=60)
+        runs = api.runs('association-rosia/crop-yield-estimate', per_page=500)
