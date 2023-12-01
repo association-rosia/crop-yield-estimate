d96c58a5f716d66cd336f10a0b5e1479b17bad65
Louis REBERGA <louis.reberga@gmail.com>
2023-12-01 21:17:53 +0100

    update delete_run.py

--- delete_run.py
+++ delete_run.py
@@ -1,14 +1,8 @@
 import wandb
 from tqdm import tqdm
-from requests.exceptions import ReadTimeout
-api = wandb.Api()
-runs = api.runs('association-rosia/crop-yield-estimate')
 
-while runs:
-    try:
-        runs = api.runs('association-rosia/crop-yield-estimate')
-        for run in tqdm(runs):
-            run.delete()
-    except ReadTimeout as e :
-        api = wandb.Api()
-        runs = api.runs('association-rosia/crop-yield-estimate')
+api = wandb.Api(timeout=600)
+runs = api.runs('association-rosia/crop-yield-estimate', per_page=1000)
+
+for run in tqdm(runs):
+    run.delete()
