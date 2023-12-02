b8d38f22d48468b321f51fd873da3c6dcbed8bdd
Louis REBERGA <louis.reberga@gmail.com>
2023-12-02 16:12:20 +0100

    bugs fixed

--- src/models/utils.py
+++ src/models/utils.py
@@ -207,7 +207,7 @@ def train_model(run_config: dict, estimator, X: np.ndarray, y: np.ndarray, cv, t
         if run_config['great_augmentation']:
             X_train, y_train = apply_great(run_config, X_train, y_train, target_transformer, preprocessor)
 
-        if run_config['smote_augmentation'] and run_config['fillna']:
+        if run_config['smote_augmentation'] and run_config['fillna'] != 'none':
             X_train, y_train = apply_smote(X_train, y_train)
 
         estimator.fit(X_train, y_train)
