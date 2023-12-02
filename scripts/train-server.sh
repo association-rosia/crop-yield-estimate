c0a53e209054aacb4cdfd4f0a7e627a5f6b9eed3
Louis REBERGA <louis.reberga@gmail.com>
2023-12-02 19:33:28 +0100

    agents = 10

---
+++ scripts/train-server.sh
@@ -0,0 +1,8 @@
+python src/models/train_model.py --estimator_name XGBoost --task classification --nb_agents 10
+python src/models/train_model.py --estimator_name XGBoost --task reg_l --nb_agents 10
+python src/models/train_model.py --estimator_name XGBoost --task reg_m --nb_agents 10
+python src/models/train_model.py --estimator_name XGBoost --task reg_h --nb_agents 10
+python src/models/train_model.py --estimator_name CatBoost --task classification --nb_agents 10
+python src/models/train_model.py --estimator_name CatBoost --task reg_l --nb_agents 10
+python src/models/train_model.py --estimator_name CatBoost --task reg_m --nb_agents 10
+python src/models/train_model.py --estimator_name CatBoost --task reg_h --nb_agents 10
