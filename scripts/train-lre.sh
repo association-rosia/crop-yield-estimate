f2e4402f6c2cc8130c864df90a813a97737bf6fb
Louis REBERGA <louis.reberga@gmail.com>
2023-12-03 18:16:44 +0100

    final predict scripts

--- scripts/train-lre.sh
+++ scripts/train-lre.sh
@@ -1,2 +1,6 @@
-python src/models/train_model.py --estimator_name LightGBM --task classification --nb_agents 5
-python src/models/train_model.py --estimator_name LightGBM --task reg_l --nb_agents 5
+#python src/models/train_model.py --estimator_name LightGBM --task classification --nb_agents 5
+#python src/models/train_model.py --estimator_name LightGBM --task reg_l --nb_agents 5
+#python src/models/train_model.py --estimator_name LightGBM --task reg_m --nb_agents 5
+#python src/models/train_model.py --estimator_name LightGBM --task reg_h --nb_agents 5
+python src/models/train_model.py --estimator_name CatBoost --task reg_m --nb_agents 6
+python src/models/train_model.py --estimator_name CatBoost --task classification --nb_agents 6
